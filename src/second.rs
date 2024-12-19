9f638476faa16785b4616eb16dccdf02951c1f30
MeMetoCoco3 <torresmartinezvidal@gmail.com>
2024-12-20 00:33:18 +0100

    Second list, an ok stack!

--- src/second.rs
+++ src/second.rs
@@ -3,7 +3,10 @@
 pub struct List<T>{
     head: Link<T>,
 }
-
+pub struct Iter<'a, T>{
+    next:Option<&'a Node<T>>,
+}
+pub struct IntoIter<T>(List<T>);
 /*
 enum Link{
     Empty,
@@ -16,6 +19,54 @@ struct Node<T>{
     elem: T,
     next: Link<T>,
 }
+
+pub trait Iterator{
+    type Item;
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+
+impl <T> List<T>{
+    pub fn into_iter(self) ->IntoIter<T>{
+        IntoIter(self)
+    }
+}
+
+pub struct IterMut<'a, T>{
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T>{
+    pub fn iter_mut(&mut self) ->IterMut<'_, T>{
+        IterMut{ next: self.head.as_deref_mut()}
+    }
+}
+
+
+// This 'a talks about the lifetime of a struct/enum/whatever, in this case means,
+// this iterator, and this data, will NOT be able to outlive the original list.
+// '_ Means that the lifetime is infered.
+impl<'a, T> Iterator for IterMut<'a, T>{
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item>{
+        self.next.take().map(|node|{
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+
+
+impl <T> Iterator for IntoIter<T>{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item>{
+        // Before we defined that IntoIter is basically a tuple struct,
+        // A tuple struct is a struct with anonymous fields,
+        // You access those fields with self.0, self.1...
+        self.0.pop()
+    }
+}
+
 //First <T> anounces generic type
 //Second <T> says, i implement this to this exact type, in this case all of them
 impl<T> List<T>{
@@ -86,6 +137,32 @@ fn peek() {
     assert_eq!(list.peek_mut(), Some(&mut 3));
 }
 
+#[test]
+fn into_iter(){
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None) 
+}
+
+#[test]
+fn iter_mut(){
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.iter_mut();
+    let mut iter = list.iter_mut();
+
+    assert_eq!(iter.next(), Some(&mut 3));
+    assert_eq!(iter.next(), Some(&mut 2));
+    assert_eq!(iter.next(), Some(&mut 1));
+
+    assert_eq!(iter.next(), None);
+}
 
 #[cfg(test)]
 mod test{
